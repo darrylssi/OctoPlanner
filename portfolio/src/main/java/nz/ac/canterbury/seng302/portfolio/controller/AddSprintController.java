4fe625deb86b83ba0c694a36eba42bce0a963402
sne69 <sne69@uclive.ac.nz>
2022-03-22 21:08:46 +1300

    Looked at how we can make the date to dd/MMM/yyyy format using project, editProjectController class, as these were similar  to addSprint class. Also looked at how to show errors in addSprint page, but it was showing errors. We also created a function to check whether the sprint dates are within the project dates. Here Sahil was a driver and Jonathan was a navigator. #feature #refactor #pair[sne69, jdt59]

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AddSprintController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/AddSprintController.java
@@ -1,16 +1,19 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.model.DateUtils;
+import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import nz.ac.canterbury.seng302.portfolio.model.Project;
 import nz.ac.canterbury.seng302.portfolio.model.Sprint;
 import nz.ac.canterbury.seng302.portfolio.service.SprintService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.validation.BindingResult;
+
+import java.text.ParseException;
 import java.util.Date;
 
 
@@ -21,20 +24,34 @@ import java.util.Date;
 public class AddSprintController {
 
     @Autowired
+    ProjectService projectService;
+
+    @Autowired
     SprintService sprintService;
+
+    @Autowired
+    private DateUtils utils;
 
     /**
      * Gets the project name and creates a new sprint label
      * @param model Used to display the project name in title
-     * @param project Used to get the project name
      * @return The sprint add page
      */
     @GetMapping("/add-sprint")
-    public String getsSprint(Model model, Project project) {
+    public String getsSprint(@PathVariable("id") int id, Model model) {
+
+        /* Add project details to the model */
+        Project project = projectService.getProjectById(id);
 
         Sprint sprint = new Sprint();
+        sprint.setParentProjectId(id);
+
         model.addAttribute("projectName", project.getName() + " - Add Sprint");
         model.addAttribute("sprintLabel", "Add Sprint - " + sprint.getLabel());
+        model.addAttribute("sprintName",  "");
+        model.addAttribute("sprintStartDate", "");
+        model.addAttribute("sprintStartDate", "");
+        model.addAttribute("sprintDescription",  "");
 
         /* Return the name of the Thymeleaf template */
         return "addSprint";
@@ -46,27 +63,32 @@ public class AddSprintController {
      * @param sprintStartDate Gets the given sprint start date
      * @param sprintEndDate Gets the given sprint end date
      * @param sprintDescription Gets the given sprint description
-     * @param project Gets the current project
      * @return To the teacherProjectDetails page
      */
     @PostMapping("/add-sprint")
     public String projectSave(
-            @RequestParam(name="sprintName") String sprintName,
-            @RequestParam(name="sprintStartDate") Date sprintStartDate,
-            @RequestParam(name="sprintEndDate") Date sprintEndDate,
-            @RequestParam(name="sprintDescription") String sprintDescription,
-            Project project,
-            BindingResult result,
+            @PathVariable("id") int id,
+            @RequestParam(value="sprintName") String sprintName,
+            @RequestParam(value="sprintStartDate") String sprintStartDate,
+            @RequestParam(value="sprintEndDate") String sprintEndDate,
+            @RequestParam(value="sprintDescription") String sprintDescription,
+            Sprint sprint,
             Model model
-    ) {
-        if (result.hasErrors()) {
+    ) throws ParseException {
+
+        Project parentProject = projectService.getProjectById(sprint.getParentProjectId());
+        if (parentProject.getStartDate().after(sprint.getStartDate())) {
             return "addSprint";
         }
 
+        sprint.setParentProjectId(parentProject.getId());
+        sprint.setSprintName(sprintName);
+        sprint.setStartDate(utils.toDate(sprintStartDate));
+        sprint.setEndDate(utils.toDate(sprintEndDate));
+        sprint.setSprintDescription(sprintDescription);
 
-        Sprint newSprint = new Sprint(project.getId(), sprintName, sprintDescription, sprintStartDate, sprintEndDate);
-        sprintService.saveOrUpdateSprint(newSprint);
-        return "teacherProjectDetails";
+        sprintService.saveOrUpdateSprint(sprint);
+        return "details";
     }
 
 }
--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/Sprint.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/model/Sprint.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Calendar;
+
+import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity // this is an entity, assumed to be in a table called Sprint
@@ -28,19 +30,14 @@ public class Sprint {
     private String sprintLabel = "Sprint ";
     private int sprintNum = 0;
 
-    @Size(max = 200, message = "The maximum lenght can be 200.")
+    @Size(max = 200, message = "The maximum length can be 200.")
     private String sprintDescription;
 
     @NotBlank
     @Column(nullable = false)
-    @Temporal(TemporalType.DATE)
-    @DateTimeFormat(pattern="dd/MMM/yyyy")
     private Date sprintStartDate;
 
-    @NotBlank
     @Column(nullable = false)
-    @Temporal(TemporalType.DATE)
-    @DateTimeFormat(pattern="dd/MMM/yyyy")
     private Date sprintEndDate;
 
     public Sprint() {}
@@ -57,8 +54,8 @@ public class Sprint {
         this.parentProjectId = parentProjectId;
         this.sprintName = sprintName;
         this.sprintDescription = sprintDescription;
-        this.sprintStartDate = Sprint.stringToDate(sprintStartDate);
-        this.sprintEndDate = Sprint.stringToDate(sprintEndDate);
+        this.sprintStartDate = Project.stringToDate(sprintStartDate);
+        this.sprintEndDate = Project.stringToDate(sprintEndDate);
     }
 
     @Override
@@ -68,36 +65,14 @@ public class Sprint {
                 id, parentProjectId, sprintName, sprintLabel, sprintStartDate, sprintEndDate, sprintDescription);
     }
 
-    /**
-     * Gets the date form of the given date string
-     *
-     * @param dateString the string to read as a date in format 01/Jan/2000
-     * @return the given date, as a date object
-     */
-    static Date stringToDate(String dateString) {
-        Date date = null;
-        try {
-            date = new SimpleDateFormat("dd/MMM/yyyy").parse(dateString);
-        } catch (Exception e) {
-            System.err.println("Error parsing date: " + e.getMessage());
-        }
-        return date;
-    }
-
-    /**
-     * Gets the string form of the given date in
-     *
-     * @param date the date to convert
-     * @return the given date, as a string in format 01/Jan/2000
-     */
-    static String dateToString(Date date) {
-        return new SimpleDateFormat("dd/MMM/yyyy").format(date);
-    }
-
     /* Getters and Setters */
 
     public int getId(){
         return  id;
+    }
+
+    public void setParentProjectId(int parentProjectId) {
+        this.parentProjectId = parentProjectId;
     }
 
     public int getParentProjectId() {
@@ -155,4 +130,26 @@ public class Sprint {
     public void setEndDateString(String date) {
         this.sprintStartDate = Project.stringToDate(date);
     }
+
+    public boolean validWithProject(Project project) {
+        try {
+            return  this.sprintStartDate.after(project.getStartDate()) &&
+                    this.sprintEndDate.before(project.getEndDate()) &&
+                    this.sprintStartDate.before(this.sprintEndDate);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+//    @AssertTrue(message = "Start date must be before end date")
+//    public boolean isPasswordsEqual() {
+//        if (this.sprintStartDate.before(this.sprintEndDate)) {
+//            return true;
+//        }
+//        return false;
+//
+//    }
+
+
+
 }
