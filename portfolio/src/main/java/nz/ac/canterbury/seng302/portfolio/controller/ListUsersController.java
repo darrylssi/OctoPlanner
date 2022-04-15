28825c1f0c935729aa28634c3a6ea4d9e33a8d80
Andrew Hall <amh284@uclive.ac.nz>
2022-04-16 11:59:45 +1200

    Implemented AC6: Prevent one user's sorting from affecting another user

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ListUsersController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/ListUsersController.java
@@ -2,6 +2,8 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.authentication.CookieUtil;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.ClaimDTO;
 import nz.ac.canterbury.seng302.shared.identityprovider.PaginatedUsersResponse;
 
 import javax.servlet.http.Cookie;
@@ -11,8 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.util.Pair;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,7 +32,7 @@ public class ListUsersController {
     private static final Logger logger = LoggerFactory.getLogger(ListUsersController.class);
 
     private static final int PAGE_SIZE = 10;
-    private static final String COOKIE_NAME = "page_order";
+    private static final String COOKIE_NAME_PREFIX = "page_order-";
     private static final String DEFAULT_ORDER_COLUMN = "name";
     private static final boolean DEFAULT_IS_ASCENDING = true;
 
@@ -43,12 +45,20 @@ public class ListUsersController {
     @GetMapping("/users")
     public String GetListOfUsers(
             @RequestParam(name="page", defaultValue="1") int page,
+            @AuthenticationPrincipal AuthState principal,
             Model model,
             HttpServletRequest request,
             HttpServletResponse response
     ) {
+        // Please for the love of god, someone make this easier
+        logger.info(principal.getClaimsList().stream().map(claim -> claim.getType() + "->" + claim.getValue()).toList().toString());
+        String userId = principal.getClaimsList().stream()
+            .filter(claim -> claim.getType().equals("nameid"))
+            .findFirst()
+            .map(ClaimDTO::getValue)
+            .orElse("-1");
         // Get sort column & direction from cookie
-        Pair<String, Boolean> ordering = getPageOrdering(request);
+        Pair<String, Boolean> ordering = getPageOrdering(request, userId);
         String orderBy = ordering.getFirst();
         boolean isAscending = ordering.getSecond();
 
@@ -58,7 +68,7 @@ public class ListUsersController {
             users = userAccountClientService.getPaginatedUsers(page - 1, PAGE_SIZE, orderBy, isAscending);
         } catch (IllegalArgumentException e) {
             // The orderBy column in the cookie is invalid, delete it
-            CookieUtil.clear(response, COOKIE_NAME);
+            deleteOrderingCookie(userId, response);
             // TODO Andrew: Throw a 400 error once George's branch is merged
             throw e;
         }
@@ -86,11 +96,18 @@ public class ListUsersController {
     public String changeUserListOrdering(
         @RequestParam(name="page", defaultValue="1") int page,
         @RequestParam(name="orderBy", required=true) String orderBy,
+        @AuthenticationPrincipal AuthState principal,
         HttpServletRequest request,
         HttpServletResponse response
     ) {
+        // Please for the love of god, someone make this easier
+        String userId = principal.getClaimsList().stream()
+            .filter(claim -> claim.getType().equals("nameid"))
+            .findFirst()
+            .map(ClaimDTO::getValue)
+            .orElse("-1");
         // Get the user's existing ordering
-        Pair<String, Boolean> ordering = getPageOrdering(request);
+        Pair<String, Boolean> ordering = getPageOrdering(request, userId);
         String savedOrderBy = ordering.getFirst();
         boolean savedIsAscending = ordering.getSecond();
 
@@ -101,11 +118,7 @@ public class ListUsersController {
             isAscending = DEFAULT_IS_ASCENDING;
         }
         // Set the cookie
-        String strDirection = (isAscending) ? "ASC" : "DESC";
-        Cookie cookie = new Cookie(COOKIE_NAME, orderBy + ':' + strDirection);
-        cookie.setPath("/users");
-        cookie.setMaxAge(365 * 24 * 60 * 60);   // Expire in a year
-        response.addCookie(cookie);
+        createOrderingCookie(orderBy, isAscending, userId, response);
 
         return "redirect:/users?page="+page;    // Send them back to the users page
     }
@@ -119,13 +132,14 @@ public class ListUsersController {
      * NOTE: This won't check if the orderBy column will be accepted by the gRPC, merely that the format's correct
      * 
      * @param request Your controller's request object
+     * @param userId The ID of the logged in user. This is to satisfy AC6
      * @return A pair of type (orderBy, isAscending) for the page.
      *         If the cookie is abscent or invalid, it'll return the default values <code>("name", true(<i>ascending</i>))</code>
      */
-    private static Pair<String, Boolean> getPageOrdering(HttpServletRequest request) {
+    private static Pair<String, Boolean> getPageOrdering(HttpServletRequest request, String userId) {
         final Pair<String, Boolean> DEFAULT_VALUES = Pair.of(DEFAULT_ORDER_COLUMN, DEFAULT_IS_ASCENDING);
         
-        String value = CookieUtil.getValue(request, COOKIE_NAME);
+        String value = CookieUtil.getValue(request, COOKIE_NAME_PREFIX+userId);
         if (value == null) {        // Cookie doesn't exist
             return DEFAULT_VALUES;
         }
@@ -146,5 +160,20 @@ public class ListUsersController {
             return DEFAULT_VALUES;
         }
         return Pair.of(orderBy, isAscending);        
+    }
+
+
+    void createOrderingCookie(String orderBy, boolean isAscending, String userId, HttpServletResponse response) {
+        String strDirection = (isAscending) ? "ASC" : "DESC";
+        // Give each user on this machine a different sorting cookie (AC6)
+        String cookieName = COOKIE_NAME_PREFIX + userId;
+        Cookie cookie = new Cookie(cookieName, orderBy + ':' + strDirection);
+        cookie.setPath("/users");
+        cookie.setMaxAge(365 * 24 * 60 * 60);   // Expire in a year
+        response.addCookie(cookie);
+    }
+
+    void deleteOrderingCookie(String userId, HttpServletResponse response) {
+        CookieUtil.clear(response, COOKIE_NAME_PREFIX+userId);
     }
 }
