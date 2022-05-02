d09e3aa2b6208963cacab74c208288cfc23a360e
Max Bastida <ieb15@uclive.ac.nz>
2022-05-02 16:42:45 +1200

    Added endpoint for adding roles to users and linked to button on users list page. User list button currently hardcoded for testing.

---
+++ portfolio/src/main/resources/static/scripts/editRoles.js
@@ -0,0 +1,9 @@
+   function addRole(userId, role) {
+    const url = "/users/" + userId + "/add-role/" + role;
+    const patchRequest = new XMLHttpRequest();
+    patchRequest.open("PATCH", url, true);
+    patchRequest.onload = () => {
+        window.location.reload();
+    }
+    patchRequest.send();
+}
