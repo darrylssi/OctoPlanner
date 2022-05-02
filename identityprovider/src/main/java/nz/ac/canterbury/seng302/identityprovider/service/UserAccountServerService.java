6a989f70b3196e219bb66c54d9625dcade6e9319
Max Bastida <ieb15@uclive.ac.nz>
2022-05-03 09:22:19 +1200

    Updated UserResponse to include user id. Add and delete role from user buttons on the list of users page can now access the user id.

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/UserAccountServerService.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/service/UserAccountServerService.java
@@ -359,6 +359,7 @@ public class UserAccountServerService extends UserAccountServiceGrpc.UserAccount
                 .setEmail(user.getEmail())
                 .setProfileImagePath("/") // TODO Path to users profile image once implemented
                 .addAllRoles(user.getRoles())
+                .setId(user.getID())
                 .setCreated(Timestamp.newBuilder()  // Converts Instant to protobuf.Timestamp
                         .setSeconds(user.getCreated().getEpochSecond())
                         .setNanos(user.getCreated().getNano()));
