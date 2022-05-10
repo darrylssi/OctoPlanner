c671090e591829d76f16fa72ed738203e7e9a2b1
Sahil Negi <sne69@uclive.ac.nz>
2022-05-10 20:44:56 +1200

    In MonthlyCalendarController class, added two function, as well as updated the @GetMapping function to send the sprint names, sprint start dates and sprint end dates. And updated the monthlyCalendar.html and monthlyCalendar.js page, so the sprints can be shown on the monthly calendar page. #feature

--- portfolio/src/main/resources/static/scripts/monthlyCalendar.js
+++ portfolio/src/main/resources/static/scripts/monthlyCalendar.js
@@ -1,18 +1,36 @@
 
 
-
 document.addEventListener('DOMContentLoaded', function() {
     let calendarEl = document.getElementById('calendar');
 
+
+    // Converting the given sprint's string names, start date and end date to list
+    let sprintNamesList = sprintNames.split(",");
+    let sprintStartDatesList = sprintStartDates.split(",");
+    let sprintEndDatesList = sprintEndDates.split(",");
+
+    // Creating one list for calendar sprints
+    let sprints = [];
+    for(let i = 0; i < sprintNamesList.length; i++) {
+        sprints.push( {title: sprintNamesList[i], start: sprintStartDatesList[i],
+            end: sprintEndDatesList[i]} )
+    };
+
+
     let calendar = new FullCalendar.Calendar(calendarEl, {
         timeZone: 'UTC',
-        defaultView: 'dayGridMonth',
+        initialView: 'dayGridMonth',
+        // Restricts the calendar dates based on the given project dates
         validRange: {
-        start: projectStartDate,
-        end: projectEndDate
-        }
+            start: projectStartDate,
+            end: projectEndDate
+        },
+        // Used to show all the sprints on the calendar
+        events: sprints
     });
+    // On startup, calendar date starts from the given project start date
     calendar.gotoDate(projectStartDate);
 
     calendar.render();
+
 });
