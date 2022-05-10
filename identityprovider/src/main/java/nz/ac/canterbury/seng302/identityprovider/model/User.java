1833e9fa5fb59884a87f444a4cb29f966bdc8954
Max Bastida <ieb15@uclive.ac.nz>
2022-05-11 10:18:08 +1200

    Updated some javadoc and fixed a bug where user roles were doubled.

--- identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/model/User.java
+++ identityprovider/src/main/java/nz/ac/canterbury/seng302/identityprovider/model/User.java
@@ -154,9 +154,9 @@ public class User {
     }
 
     /**
-     * Add a role to this user
+     * Remove a role from this user
      * 
-     * @param role The role enum type to be added
+     * @param role The role enum type to be removed
      * @return <code>true</code> if the user had this item removed
      */
     public boolean removeRole(UserRole role) {
