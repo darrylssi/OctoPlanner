f5fa803bc868876f93d2079411f54bb6266d10d4
jdt59 <jdt59@uclive.ac.nz>
2022-05-11 16:21:42 +1200

    Sprint text colours on the calendar are now determined based on the background colour so that it is always readable.

--- portfolio/src/main/resources/static/scripts/monthlyCalendar.js
+++ portfolio/src/main/resources/static/scripts/monthlyCalendar.js
@@ -1,4 +1,17 @@
 
+// Will return black or white based on the provided colour string
+// so that text shows up
+// based on https://www.w3.org/TR/AERT/#color-contrast
+// and https://stackoverflow.com/questions/11867545/change-text-color-based-on-brightness-of-the-covered-background-area
+// (Alex Ball's answer)
+function getTextColour(hexColourString) {
+    let r = parseInt(hexColourString.slice(1, 3), 16);
+    let g = parseInt(hexColourString.slice(3, 5), 16);
+    let b = parseInt(hexColourString.slice(5, 7), 16);
+
+    let brightness = Math.round((r * 299 + g * 587 + b * 114) / 1000);
+    return (brightness > 125) ? 'black' : 'white';
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     let calendarEl = document.getElementById('calendar');
@@ -15,10 +28,8 @@ document.addEventListener('DOMContentLoaded', function() {
     let sprints = [];
     // let colours = ["cornflowerblue", "firebrick", "forestgreen", "blueviolet", "tomato", "darkslategrey", "darkorchid"];
     for(let i = 0; i < sprintNamesList.length; i++) {
-        // sprints.push( {title: sprintNamesList[i], start: sprintStartDatesList[i],
-        //     end: sprintEndDatesList[i], backgroundColor: colours[i % colours.length]} )
         sprints.push( {title: sprintNamesList[i], start: sprintStartDatesList[i],
-            end: sprintEndDatesList[i], backgroundColor: sprintColoursList[i]} )
+            end: sprintEndDatesList[i], backgroundColor: sprintColoursList[i], textColor: getTextColour(sprintColoursList[i])})
     }
 
 
