01587009897a348c872ac3e76b511b258aa95e2c
Sahil Negi <sne69@uclive.ac.nz>
2022-05-23 21:37:51 +1200

    Tried working on adding the roles to the users.html page, but dropdown button for that is not working correctly, instead it is not showing. This is also causing the header's dropdown menu to now work. #feature

--- portfolio/src/main/resources/static/scripts/editRoles.js
+++ portfolio/src/main/resources/static/scripts/editRoles.js
@@ -1,13 +1,31 @@
 
-function addRole(userId, role) {
-    const url = BASE_URL + "users/" + userId + "/add-role/" + role;
-    const patchRequest = new XMLHttpRequest();
-    patchRequest.open("PATCH", url, true);
-    patchRequest.onload = () => {
-        window.location.reload();
-    }
-    patchRequest.send();
+function addRoleFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
 }
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.add-role-button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+// function addRole(userId, role) {
+//     const url = BASE_URL + "users/" + userId + "/add-role/" + role;
+//     const patchRequest = new XMLHttpRequest();
+//     patchRequest.open("PATCH", url, true);
+//     patchRequest.onload = () => {
+//         window.location.reload();
+//     }
+//     patchRequest.send();
+// }
 
 function removeRole(userId, role) {
     const url = BASE_URL + "/users/" + userId + "/remove-role/" + role;
