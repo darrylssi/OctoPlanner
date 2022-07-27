9dc93dfdd16ab6f790a3da6977416d58f757c963
George Hampton <gha95@uclive.ac.nz>
2022-07-27 16:39:16 +1200

    Revert "Merge branch 'dev' into 'master'"
    This reverts merge request !74

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditUserController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/EditUserController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import nz.ac.canterbury.seng302.shared.util.ValidationError;
 
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
