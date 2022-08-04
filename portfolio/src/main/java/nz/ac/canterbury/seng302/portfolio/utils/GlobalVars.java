30a1b7961f4565389a361baa35946392063b15ed
Darryl Alang <daa59@uclive.ac.nz>
2022-08-04 16:49:04 +1200

    Refactored Event, Project and Sprint models to use variables instead of hard-coded literals.

---
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/utils/GlobalVars.java
@@ -0,0 +1,18 @@
+package nz.ac.canterbury.seng302.portfolio.utils;
+
+public class GlobalVars {
+
+    private GlobalVars() {}
+
+    public static final int MAX_DESC_LENGTH = 200;
+
+    public static final int MIN_NAME_LENGTH = 2;
+    public static final int MAX_NAME_LENGTH = 32;
+
+    public static final int COLOUR_LENGTH = 7;
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DISPLAY_DATE_FORMAT = "dd/MMM/yyyy";
+    public static final String DATETIME_FORMAT = "dd/MMM/yyyy HH:mm:ss";
+
+}
