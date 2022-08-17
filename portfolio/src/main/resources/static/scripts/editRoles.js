f9de68e171c0c5f5fb56e95f220a2654c6f034e7
jdt59 <jdt59@uclive.ac.nz>
2022-08-17 13:15:38 +1200

    When adding roles to a user, only one dropdown can be visible at a time now. Tried to hide the add role button when no new roles could be added. It should only be visible if there is a role in acceptableRoles that is NOT in user.getRolesList, but I don't know how to check that just with Thymeleaf. I thought about doing it with JS, but you can't do that in a th:if statement.

--- portfolio/src/main/resources/static/scripts/editRoles.js
+++ portfolio/src/main/resources/static/scripts/editRoles.js
@@ -62,10 +62,38 @@ function addRole(userId, role) {
     patchRequest.send();
 }
 
-/** Toggles the dropdown with the associated id
- *
+/**
+ * Toggles the dropdown with the associated id
  * @param elementId the id of the dropdown to be toggled
  */
 function toggleRoleList(elementId) {
+    hideAllRoleDropdowns();
     document.getElementById(elementId).classList.toggle("show");
+}
+
+/**
+ * Does what it says on the tin - hides all add role dropdowns when called.
+ */
+function hideAllRoleDropdowns() {
+    const dropdowns = document.getElementsByClassName("user-role-dropdown");
+    for (const item of dropdowns) {
+        item.classList.remove("show");
+    }
+}
+
+/**
+ * Returns true if there is a role in acceptableRoles that is not within targetUserRoles.
+ * This would be used to check whether the add roles button should be visible, but you can't call JS from a th:if
+ * statement. So, TODO remove this if it is not usable.
+ * @param acceptableRoles
+ * @param targetUserRoles
+ * @return true if an element in acceptableRoles is not in targetRoles, else false
+ */
+function canAddRole(acceptableRoles, targetUserRoles) {
+    for (const item of acceptableRoles) {
+        if (!item in targetUserRoles) {
+            return true;
+        }
+    }
+    return false;
 }
