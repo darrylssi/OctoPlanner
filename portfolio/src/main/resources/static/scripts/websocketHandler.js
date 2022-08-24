b0a2c5319fbe029c9c4777b20164a6fd5fe05ea2
Jonathan Tomlinson <jdt59@uclive.ac.nz>
2022-08-24 17:01:11 +1200

    Removed some old/unused code from websocketHandler.js and created new functions for dealing with editing messages for schedulables, not just events. They aren't linked up to anything yet and I'll need to do some refactoring elsewhere to get that happening.

--- portfolio/src/main/resources/static/scripts/websocketHandler.js
+++ portfolio/src/main/resources/static/scripts/websocketHandler.js
@@ -2,17 +2,8 @@ let stompClient = null;
 const eventTimeouts = new Map(); // holds event ids and setTimeout functions in a key/value pair mapping
 const EVENT_EDIT_MESSAGE_TIMEOUT = 4000; // hide editing event messages after this many ms
 
-/**
- * Sets html elements according to whether a WebSocket has been connected to or not
- * @param connected Boolean if a connection has been established to a WebSocket
- */
-function setConnected(connected) {
-    // Currently, does not contain any elements that need to be set according to the connected status
-    let response = document.getElementById('response');
-    if (response !== null ) {
-        response.innerHTML = '';
-    }
-}
+const schedulableTimeouts = new Map(); // holds schedulable box classes and setTimeout functions in a key/value pair mapping
+const SCHEDULABLE_EDIT_MESSAGE_TIMEOUT = 4000; // hide editing schedulable messages after this many ms
 
 /**
  * Sets up a connection to a WebSocket
@@ -26,6 +17,7 @@ function connect() {
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/editing-event', function(message) {
             handleEventMessage(JSON.parse(message.body));
+//          TODO  handleEditingSchedulableMessage(JSON.parse(message.body));
         });
     });
 }
@@ -40,6 +32,108 @@ function disconnect() {
         setConnected(false);
         console.log("Disconnected");
 }
+
+/**
+ * Sends a message saying that a user is editing the specified schedulable.
+ * Format: `schedulableType,schedulableId,userId'.
+ */
+function sendEditingSchedulableMessage(schedulableId, schedulableType) {
+    console.log("SENDING EDITING MESSAGE FOR SCHEDULABLE: " + schedulableId)
+    let user = document.getElementById('user').getAttribute('data-name');
+    let userId = document.getElementById('userId').getAttribute('data-name');
+    let content = `${schedulableId},${schedulableType},${userId}`;
+    stompClient.send("/app/ws", {},
+    JSON.stringify({'from':user, 'content':content}));
+}
+
+/**
+ * Sends a websocket message saying that a user has stopped editing a schedulable.
+ * Won't send anything if schedulableId is undefined
+ */
+function sendStopEditingMessage(schedulableId, schedulableType) {
+    if (previousSchedulable !== undefined) {
+        console.log('SENDING STOP MESSAGE FOR SCHEDULABLE: ' + schedulableId);
+        let user = document.getElementById('user').getAttribute('data-name');
+        stompClient.send("/app/ws", {},
+        JSON.stringify({'from':user, 'content':`${schedulableId},${schedulableType}`}));
+    }
+}
+
+/**
+ * Decides whether the schedulable message means to show editing or hide editing.
+ * @param editMessage JSON object received from the WebSocket
+ */
+function handleEditingSchedulableMessage(editMessage) {
+    messageContent = editMessage.content.split(',');
+    if (messageContent.length === 3) {
+        console.log('GOT EDITING MESSAGE ' + editMessage.content);
+        const schedulableId = messageContent[0];
+        const schedulableType = messageContent[1].toLowerCase();
+        const userId = messageContent[2];
+        showEditingSchedulableMessage(schedulableId, schedulableType, userId);
+    } else if (messageContent.length() == 2) {
+        console.log('GOT STOP MESSAGE ' + editMessage.content);
+        const schedulableId = messageContent[0];
+        const schedulableType = messageContent[1].toLowerCase();
+        hideEditingSchedulableMessage(schedulableId, schedulableType);
+    }
+}
+
+/**
+ * Shows editing-schedulable notifications on the page
+ * @param editMessage JSON object received from the WebSocket
+ */
+function showEditingSchedulableMessage(schedulableId, schedulableType, userId) {
+    const docUserId = document.getElementById("userId").getAttribute('data-name'); // the id of the user on this page
+
+    if (userId !== docUserId) {
+        // stops any existing timeouts so that the message is shown for the full length
+        stopSchedulableTimeout(schedulableId);
+
+        // locate the correct elements on the page
+        const editingSchedulableBoxClass = `${schedulableType}-${schedulableId}-editing-box`;
+        const editingSchedulableTextBoxClass = `${schedulableType}-${schedulableId}-editing-text`;
+        const editingSchedulableBoxes = document.getElementsByClassName(editingSchedulableBoxClass);
+        const editingSchedulableTextBoxes = document.getElementsByClassName(editingSchedulableTextBoxClass);
+
+        // update the text and make it visible
+        for (const schedulableTextBox of editingSchedulableTextBoxes) {
+            if (schedulableTextBox) {
+                schedulableTextBox.innerHTML = `${username} is editing this ${schedulableType}`;
+            }
+        }
+        for (const schedulableBox of editingSchedulableBoxes) {
+            if (schedulableBox) {
+                schedulableBox.style.visibility = "visible";
+            }
+        }
+
+        // Hide it after 8s
+        schedulableTimeouts.set(editingSchedulableBoxClass, setTimeout(function() {hideEditingSchedulableMessage(schedulableId, schedulableType)}, SCHEDULABLE_EDIT_MESSAGE_TIMEOUT));
+    }
+}
+
+/**
+ * Hides the editing message for the specified schedulable and clears the timer running for it.
+ * Will clear the messages from all schedulable boxes for that schedulable (such as if it spans many sprints).
+ * @param message the stop message that was received
+ */
+function hideEditingSchedulableMessage(schedulableId, schedulableType) {
+    // this check is so that if you are editing a schedulable that someone else is editing, you don't hide their message
+    // when you close your form. Their message would reappear without this anyway but it avoids confusion.
+    if (document.getElementById('user').getAttribute('data-name') !== message.from) {
+        const editingSchedulableBoxId = `${schedulableType}-${schedulableId}-editing-box`;
+        const editingSchedulableBoxes = document.getElementsByClassName(editingSchedulableBoxId);
+
+        for (const schedulableBox of editingSchedulableBoxes) {
+            if (schedulableBox) {
+                schedulableBox.style.visibility = "hidden";
+            }
+        }
+        stopEditingSchedulableTimeout(editingSchedulableBoxId);
+    }
+}
+
 
 /**
  * Sends a message to a WebSocket endpoint using data from an HTML element
@@ -64,19 +158,6 @@ function sendStopEditingMessage(eventId) {
         stompClient.send("/app/ws", {},
         JSON.stringify({'from':user, 'content':eventId}));
     }
-}
-
-/**
- * Updates an HTML element to display a received WebSocket message
- * @param messageOutput JSON object received from the WebSocket
- */
-function showMessageOutput(messageOutput) {
-    const response = document.getElementById('response');
-    const p = document.createElement('p');
-    p.style.wordWrap = 'break-word';
-    p.appendChild(document.createTextNode(messageOutput.from +
-        messageOutput.text + " (" + messageOutput.time + ")"));
-    response.appendChild(p);
 }
 
 /**
