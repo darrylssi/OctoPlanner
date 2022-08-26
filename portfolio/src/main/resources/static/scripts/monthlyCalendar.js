2534b65b8883b8548d7ab5b26f068bb2a88343a3
Jonathan Tomlinson <jdt59@uclive.ac.nz>
2022-08-26 14:21:48 +1200

    Tried to show sprints and the event icon things on the calendar, but ran into errors.

--- portfolio/src/main/resources/static/scripts/monthlyCalendar.js
+++ portfolio/src/main/resources/static/scripts/monthlyCalendar.js
@@ -2,6 +2,26 @@ const milestoneIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height
 const deadlineIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hourglass-split" viewBox="0 0 16 16"><path d="M2.5 15a.5.5 0 1 1 0-1h1v-1a4.5 4.5 0 0 1 2.557-4.06c.29-.139.443-.377.443-.59v-.7c0-.213-.154-.451-.443-.59A4.5 4.5 0 0 1 3.5 3V2h-1a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-1v1a4.5 4.5 0 0 1-2.557 4.06c-.29.139-.443.377-.443.59v.7c0 .213.154.451.443.59A4.5 4.5 0 0 1 12.5 13v1h1a.5.5 0 0 1 0 1h-11zm2-13v1c0 .537.12 1.045.337 1.5h6.326c.216-.455.337-.963.337-1.5V2h-7zm3 6.35c0 .701-.478 1.236-1.011 1.492A3.5 3.5 0 0 0 4.5 13s.866-1.299 3-1.48V8.35zm1 0v3.17c2.134.181 3 1.48 3 1.48a3.5 3.5 0 0 0-1.989-3.158C8.978 9.586 8.5 9.052 8.5 8.351z"/></svg>';
 const eventIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-event" viewBox="0 0 16 16"> <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/></svg>';
 
+let events = [,
+                         {
+                           id: 'b',
+                           title: 'deadlines',
+                           start: '2022-08-26',
+                           type: 'deadline'
+                         },
+                         {
+                           id: 'a',
+                           title: 'milestones',
+                           start: '2022-08-26',
+                           type: 'milestone'
+                         },
+                         {
+                           id: 'c',
+                           title: 'events',
+                           start: '2022-08-26',
+                           type: 'event'
+                         }
+                     ];
 
 // Will return black or white based on the provided colour string
 // so that text shows up
@@ -33,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for(let i = 0; i < sprintNamesList.length; i++) {
         sprints.push( {id: sprintIdsList[i], title: sprintNamesList[i], start: sprintStartDatesList[i],
             end: sprintEndDatesList[i], backgroundColor: sprintColoursList[i], textColor: getTextColour(sprintColoursList[i]),
+            type: 'sprint',
             classNames: 'defaultEventBorder'})
     }
     let selectedSprint = null;
@@ -79,7 +100,7 @@ document.addEventListener('DOMContentLoaded', function() {
             + " must not overlap with " + stillEvent.title.toString();
         },
         eventClick: function(info) {
-            if (sprintsEditable === "true") {
+            if (sprintsEditable === "true" && info.event.type === 'sprint') {
                 // if the user clicks on a sprint, update the selected sprint
                 if (selectedSprint != null) {
                     // removing previous sprint border colour and width
@@ -104,7 +125,7 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         eventResize: function(info) {
             // if the user clicks on a sprint, update the selected sprint
-            if (sprintsEditable === "true") {
+            if (sprintsEditable === "true" && info.event.type === 'sprint') {
                 // getting the form element by its id
                 const form  = document.getElementById('sprintForm');
 
@@ -121,26 +142,7 @@ document.addEventListener('DOMContentLoaded', function() {
         eventMouseEnter: function () {mouseoverSprint = true},
         eventMouseLeave: function() {mouseoverSprint = false},
         // Used to show all the sprints on the calendar
-        events: [sprints,
-            {
-              id: 'b',
-              title: 'deadlines',
-              start: '2022-08-26',
-              type: 'deadline'
-            },
-            {
-              id: 'a',
-              title: 'milestones',
-              start: '2022-08-26',
-              type: 'milestone'
-            },
-            {
-              id: 'c',
-              title: 'events',
-              start: '2022-08-26',
-              type: 'event'
-            }
-        ],
+        events: sprints.concat(events),
         eventContent: function(arg) {
 //            let icon = document.createElement('i')
 
@@ -155,7 +157,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     return { html: deadlineIcon }
                     break;
                 default:
-                    return;
+                    return {html: arg.event.el.innerHTML};
             }
 //                return { html: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16"> <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg>' }
 //            let arrayOfDomNodes = [ italicEl ]
