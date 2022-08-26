49c39d1a16971477fd4700e129add35198de18b9
George Hampton <gha95@uclive.ac.nz>
2022-08-26 00:32:52 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- portfolio/src/main/resources/static/scripts/websocketHandler.js
+++ portfolio/src/main/resources/static/scripts/websocketHandler.js
@@ -3,7 +3,7 @@ const schedulableTimeouts = new Map(); // holds schedulable ids and setTimeout f
 const SCHEDULABLE_EDIT_MESSAGE_TIMEOUT = 4000; // hide editing schedulable messages after this many ms
 
 // logging consts to hide certain things while developing
-const editingLogs = true;
+const editingLogs = false;
 const updateLogs = true;
 
 /**
