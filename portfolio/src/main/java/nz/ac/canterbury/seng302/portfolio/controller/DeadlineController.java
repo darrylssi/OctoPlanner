172f91b0782b2d4f36c0ff0aa77e1904bb1a2ce9
Sahil Negi <sne69@uclive.ac.nz>
2022-09-12 15:41:01 +1200

    Fixed the code using reviewed feedback. The fixes included removing unused code, a method is created for duplicate code, etc. #feature #refacter

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/DeadlineController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/DeadlineController.java
@@ -36,11 +36,10 @@ public class DeadlineController extends PageController {
      * Post request to add a deadline to a project.
      * @param principal Authenticated user
      * @param projectId ID of the project the deadline will be added to
-     * @param deadlineForm The form submitted by our lovely customers
+     * @param deadlineForm Form that stores information about the deadline
      * @param bindingResult Any errors that came up during validation
-     * @return  A response of either 200 (success), 403 (forbidden),
-     *          or 400 (Given event failed validation, replies with what errors occurred)
-
+     * @return A response of either 200 (success), 403 (forbidden),
+     *         or 400 (Given deadline failed validation, replies with what errors occurred)
      */
     @PostMapping("/project/{project_id}/add-deadline")
     public ResponseEntity<String> postAddDeadline(
@@ -57,9 +56,8 @@ public class DeadlineController extends PageController {
             return new ResponseEntity<>(ex.getReason(), ex.getStatus());
         }
 
-        ValidationError dateErrors = null;
-        ValidationError nameErrors = null;
         // Pattern: Don't do the deeper validation if the data has no integrity (i.e. has nulls)
+        Project parentProject = projectService.getProjectById(projectId);
         if (bindingResult.hasErrors()) {
             StringJoiner errors = new StringJoiner("\n");
             for (var err: bindingResult.getAllErrors()) {
@@ -68,20 +66,11 @@ public class DeadlineController extends PageController {
             return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
         }
 
-        // Check that the dates are correct
-        Project parentProject = projectService.getProjectById(projectId);
-        dateErrors = ValidationUtils.validateDeadlineDate(deadlineForm.datetimeToDate(userTimezone), parentProject);
-        nameErrors = ValidationUtils.validateName(deadlineForm.getName());
-        if (dateErrors.isError() || nameErrors.isError()) {
-            StringJoiner errors = new StringJoiner("\n");
-            for (var err: dateErrors.getErrorMessages()) {
-                errors.add(err);
-            }
-            for (var err: nameErrors.getErrorMessages()) {
-                errors.add(err);
-            }
-            return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
-        }
+        // Validation round 2: Do our custom errors pass?
+        var dateErrors = ValidationUtils.validateDeadlineDate(deadlineForm.datetimeToDate(userTimezone), parentProject);
+        var nameError = ValidationUtils.validateName(deadlineForm.getName());
+        ResponseEntity<String> errors = validateDateAndName(dateErrors, nameError);
+        if (errors != null) return errors;
 
         // Data is valid, add it to database
         Deadline deadline = new Deadline(deadlineForm.getName(), deadlineForm.getDescription(), deadlineForm.datetimeToDate(userTimezone));
@@ -102,7 +91,13 @@ public class DeadlineController extends PageController {
             @AuthenticationPrincipal AuthState principal,
             @PathVariable(name="deadlineId") int deadlineId
     ) {
-        requiresRoleOfAtLeast(UserRole.TEACHER, principal);
+        // Check if the user is authorised for this
+        try {
+            requiresRoleOfAtLeast(UserRole.TEACHER, principal);
+        } catch (ResponseStatusException ex) {
+            return new ResponseEntity<>(ex.getReason(), ex.getStatus());
+        }
+
         try {
             deadlineService.deleteDeadline(deadlineId);
             return new ResponseEntity<>("Deadline deleted.", HttpStatus.OK);
@@ -116,9 +111,10 @@ public class DeadlineController extends PageController {
      * @param principal Authenticated user
      * @param projectId ID of the project the deadline belongs to
      * @param deadlineId ID of the deadline to be edited
-     * @param deadlineForm Deadline DTO
+     * @param deadlineForm Form that stores information about the deadline
      * @param userTimeZone Current timezone of the user
-     * @return Project details page
+     * @return A response of either 200 (success), 403 (forbidden),
+     *         or 400 (Given deadline failed validation, replies with what errors occurred)
      */
     @PostMapping("/project/{project_id}/edit-deadline/{deadline_id}")
     public ResponseEntity<String> postEditDeadline(
@@ -149,16 +145,8 @@ public class DeadlineController extends PageController {
         // Validation round 2: Do our custom errors pass?
         var dateErrors = ValidationUtils.validateDeadlineDate(deadlineForm.datetimeToDate(userTimeZone), deadline.getParentProject());
         var nameError = ValidationUtils.validateName(deadlineForm.getName());
-        if (dateErrors.isError() || nameError.isError()) {
-            StringJoiner errors = new StringJoiner("\n");
-            for (var err: dateErrors.getErrorMessages()) {
-                errors.add(err);
-            }
-            for (var err: nameError.getErrorMessages()) {
-                errors.add(err);
-            }
-            return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<String> errors = validateDateAndName(dateErrors, nameError);
+        if (errors != null) return errors;
 
         deadline.setName(deadlineForm.getName());
         deadline.setDescription(deadlineForm.getDescription());
@@ -169,5 +157,23 @@ public class DeadlineController extends PageController {
         return ResponseEntity.ok("");
     }
 
-
-}
+    /**
+     * Validation for deadline's date and name.
+     * @param dateErrors Checks custom date error
+     * @param nameError Checks custom name error
+     * @return A response of either null, 200 (success),
+     *          or 400 (Given deadline failed validation, replies with what errors occurred)
+     */
+    private ResponseEntity<String> validateDateAndName(ValidationError dateErrors, ValidationError nameError) {
+        if (dateErrors.isError() || nameError.isError()) {
+            StringJoiner errors = new StringJoiner("\n");
+            for (var err: dateErrors.getErrorMessages()) {
+                errors.add(err);
+            }
+            for (var err: nameError.getErrorMessages()) {
+                errors.add(err);
+            }
+            return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }}
