01d00c4a534ce66b6d1e9ef212de22988d5d4af3
Max Bastida <ieb15@uclive.ac.nz>
2022-09-21 22:36:08 +1200

    Removed repeated function (result of merge conflict).

--- portfolio/src/main/resources/static/scripts/monthlyCalendar.js
+++ portfolio/src/main/resources/static/scripts/monthlyCalendar.js
@@ -70,20 +70,6 @@ function getSchedulableIconInfo() {
     return icons;
 }
 
-// Will return black or white based on the provided colour string
-// so that text shows up
-// based on https://www.w3.org/TR/AERT/#color-contrast
-// and https://stackoverflow.com/questions/11867545/change-text-color-based-on-brightness-of-the-covered-background-area
-// (Alex Ball's answer)
-function getTextColour(hexColourString) {
-    let r = parseInt(hexColourString.slice(1, 3), 16);
-    let g = parseInt(hexColourString.slice(3, 5), 16);
-    let b = parseInt(hexColourString.slice(5, 7), 16);
-
-    let brightness = Math.round((r * 299 + g * 587 + b * 114) / 1000);
-    return (brightness > 125) ? 'black' : 'white';
-}
-
 
 /**
  * Grabs all the sprint info from the HTML template and creates a list of FullCalendar events from it.
