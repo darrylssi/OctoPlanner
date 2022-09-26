8a0ae358079cfe106b752d9ba456429b951be7dc
Max Bastida <ieb15@uclive.ac.nz>
2022-09-27 12:31:57 +1300

    Created an endpoint for adding a list of users to a group.

--- portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupController.java
+++ portfolio/src/main/java/nz/ac/canterbury/seng302/portfolio/controller/GroupController.java
@@ -1,10 +1,26 @@
 package nz.ac.canterbury.seng302.portfolio.controller;
 
+import nz.ac.canterbury.seng302.portfolio.model.User;
+import nz.ac.canterbury.seng302.portfolio.service.GroupClientService;
+import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.shared.identityprovider.AddGroupMembersResponse;
 import nz.ac.canterbury.seng302.shared.identityprovider.AuthState;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserResponse;
+import nz.ac.canterbury.seng302.shared.identityprovider.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Controller to handle requests on the groups page.
@@ -14,6 +30,12 @@ public class GroupController extends PageController{
 
     //TODO update this when the template is actually created or just get rid of the TODO
     public static final String GROUPS_TEMPLATE_NAME = "groups";
+
+    @Autowired
+    UserAccountClientService userAccountClientService;
+
+    @Autowired
+    GroupClientService groupClientService;
 
     /**
      * Get request to view the groups page.
@@ -30,5 +52,37 @@ public class GroupController extends PageController{
         Insert code that does things with the page here
          */
         return GROUPS_TEMPLATE_NAME;    // Return the name of the Thymeleaf template
+    }
+
+    @PostMapping("/groups/{group_id}/add-members")
+    public ResponseEntity<String> addMembers(
+            @AuthenticationPrincipal AuthState principal,
+            @PathVariable("group_id") int groupId,
+            @RequestParam("user_id") int[] userIds
+    ){
+        try {
+            requiresRoleOfAtLeast(UserRole.TEACHER, principal);
+        } catch (ResponseStatusException ex) {
+            return new ResponseEntity<>(ex.getReason(), ex.getStatus());
+        }
+        List<UserResponse> usersInGroup = groupClientService.getGroupDetails(groupId).getMembersList();
+        List<Integer> idsToAdd = new ArrayList<>();
+
+        for (int userId: userIds) {
+            UserResponse userResponse = userAccountClientService.getUserAccountById(userId);
+            if(userResponse == null){
+                return new ResponseEntity<>("One or more users do not exist.", HttpStatus.NOT_FOUND);
+            }else if(!usersInGroup.contains(userResponse)) {
+                idsToAdd.add(userId);
+            }
+        }
+
+        AddGroupMembersResponse addGroupMembersResponse = groupClientService.addGroupMembers(groupId, idsToAdd);
+
+        if (addGroupMembersResponse.getIsSuccess()) {
+            return new ResponseEntity<>(addGroupMembersResponse.getMessage(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(addGroupMembersResponse.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 }
