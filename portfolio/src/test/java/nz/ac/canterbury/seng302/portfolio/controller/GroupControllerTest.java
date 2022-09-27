516d578daac246f961b669e179903c57680b278c
jdt59 <jdt59@uclive.ac.nz>
2022-09-28 11:53:53 +1300

    removing import statement...

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/GroupControllerTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/GroupControllerTest.java
@@ -18,7 +18,6 @@ import java.util.List;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
