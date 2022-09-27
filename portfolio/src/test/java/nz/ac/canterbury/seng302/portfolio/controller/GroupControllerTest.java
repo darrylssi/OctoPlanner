5f44d329495e05911401e1f804660e947a3ed093
Jacques Terblanche <jte52@uclive.ac.nz>
2022-09-28 12:04:33 +1300

    Figured out how to test grpc with mockMvc. One test case added, more to follow.

--- portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/GroupControllerTest.java
+++ portfolio/src/test/java/nz/ac/canterbury/seng302/portfolio/controller/GroupControllerTest.java
@@ -2,11 +2,14 @@ package nz.ac.canterbury.seng302.portfolio.controller;
 
 import nz.ac.canterbury.seng302.portfolio.annotation.WithMockPrincipal;
 import nz.ac.canterbury.seng302.portfolio.controller.forms.GroupForm;
+import nz.ac.canterbury.seng302.portfolio.model.Event;
 import nz.ac.canterbury.seng302.portfolio.model.Group;
 import nz.ac.canterbury.seng302.portfolio.model.Project;
 import nz.ac.canterbury.seng302.portfolio.service.GroupClientService;
 import nz.ac.canterbury.seng302.portfolio.service.ProjectService;
 import nz.ac.canterbury.seng302.portfolio.service.UserAccountClientService;
+import nz.ac.canterbury.seng302.portfolio.utils.DateUtils;
+import nz.ac.canterbury.seng302.shared.identityprovider.ModifyGroupDetailsResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,6 +23,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static nz.ac.canterbury.seng302.shared.identityprovider.UserRole.STUDENT;
 import static nz.ac.canterbury.seng302.shared.identityprovider.UserRole.TEACHER;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,7 +36,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = GroupController.class)
 @AutoConfigureMockMvc(addFilters = false)
-public class GroupControllerTest {
+class GroupControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -42,6 +48,7 @@ public class GroupControllerTest {
     @MockBean
     private UserAccountClientService userAccountClientService;
 
+    static final int GROUP_ID = 2;
     private GroupForm groupForm;                                // Initialises the group form object
     private Group group;                                        // Initialises the group object
 
@@ -57,15 +64,16 @@ public class GroupControllerTest {
 
         // Creates and sets the details to the group object
         group = new Group();
-        group.setId(1);
+        group.setId(GROUP_ID);
         group.setParentProject(parentProject);
+
+        when(projectService.getProjectById(0)).
+                thenReturn(group.getParentProject());
     }
 
     @Test
     @WithMockPrincipal(TEACHER)
     void addValidGroupAsTeacher_get200Response() throws Exception {
-        Mockito.when(projectService.getProjectById(0)).
-                thenReturn(group.getParentProject());
         mockMvc.perform(post("/project/0/add-group")
                         .param("shortName", "Test Group 1")
                         .param("longName", "Test Project Group"))
@@ -75,8 +83,6 @@ public class GroupControllerTest {
     @Test
     @WithMockPrincipal(TEACHER)
     void addBlankShortNameGroupAsTeacher_get400Response() throws Exception {
-        Mockito.when(projectService.getProjectById(0)).
-                thenReturn(group.getParentProject());
         String resultString = mockMvc.perform(post("/project/0/add-group")
                         .param("shortName", "")
                         .param("longName", "Test Project Group 2022"))
@@ -89,8 +95,6 @@ public class GroupControllerTest {
     @Test
     @WithMockPrincipal(TEACHER)
     void addBlankLongNameGroupAsTeacher_get400Response() throws Exception {
-        Mockito.when(projectService.getProjectById(0)).
-                thenReturn(group.getParentProject());
         String resultString = mockMvc.perform(post("/project/0/add-group")
                         .param("shortName", "Test Group")
                         .param("longName", ""))
@@ -103,8 +107,6 @@ public class GroupControllerTest {
     @Test
     @WithMockPrincipal(TEACHER)
     void addInvalidShortNameGroupAsTeacher_get400Response() throws Exception {
-        Mockito.when(projectService.getProjectById(0)).
-                thenReturn(group.getParentProject());
         mockMvc.perform(post("/project/0/add-group")
                         .param("shortName", "Test Group 🏋")
                         .param("longName", "Test Project Group 2022"))
@@ -115,8 +117,6 @@ public class GroupControllerTest {
     @Test
     @WithMockPrincipal(TEACHER)
     void addInvalidLongNameGroupAsTeacher_get400Response() throws Exception {
-        Mockito.when(projectService.getProjectById(0)).
-                thenReturn(group.getParentProject());
         mockMvc.perform(post("/project/0/add-group")
                         .param("shortName", "Test Group")
                         .param("longName", "Test Project Group 2022 🏋"))
@@ -132,4 +132,23 @@ public class GroupControllerTest {
                 .andExpect(content().string("You do not have permission to access this endpoint"));
     }
 
+    @Test
+    @WithMockPrincipal(TEACHER)
+    void editValidGroupAsTeacher_get200Response() throws Exception {
+        String shortName = "Test Valid Short Name";
+        String longName = "Test Valid Long Name";
+        ModifyGroupDetailsResponse response = ModifyGroupDetailsResponse.newBuilder()
+                .setIsSuccess(true)
+                .setMessage("Group edited successfully")
+                .build();
+        when(groupClientService.modifyGroupDetails(GROUP_ID,
+                shortName,longName)).thenReturn(response);
+
+        this.mockMvc.perform(post("/project/0/edit-group/" + GROUP_ID)
+                        .param("shortName", shortName)
+                        .param("longName", longName))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Group edited successfully"));
+        verify(groupClientService).modifyGroupDetails(GROUP_ID, shortName, longName);
+    }
 }
