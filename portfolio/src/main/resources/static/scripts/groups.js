5699be000a81f3dd84473fc905213f5578252a53
Jonathan Tomlinson <jdt59@uclive.ac.nz>
2022-09-28 15:31:57 +1300

    Fixed name of function that I changed accidentally...

--- portfolio/src/main/resources/static/scripts/groups.js
+++ portfolio/src/main/resources/static/scripts/groups.js
@@ -2,7 +2,7 @@
  * Swaps the visible and hidden text attributes for the specified group's show/hide users button.
  * @param button the button element to swap text for
  */
-function toggleUsersButtonBottom(button) {
+function toggleUsersButton(button) {
     const currentText = button.innerHTML;
     button.innerHTML=button.getAttribute("showText");
     button.setAttribute("showText", currentText);
