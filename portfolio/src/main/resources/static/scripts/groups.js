0196451390a4eb35ecb2662413ccbad558d8e4f4
Sahil Negi <sne69@uclive.ac.nz>
2022-09-28 23:41:15 +1300

    Added a function to groups.js, so the user can create group by interacting with add group form GUI. #feature

--- portfolio/src/main/resources/static/scripts/groups.js
+++ portfolio/src/main/resources/static/scripts/groups.js
@@ -15,3 +15,38 @@ function toggleUsersButton(button) {
 function toggleById(group_id) {
     toggleUsersButton(document.getElementById("user-button-" + group_id));
 }
+
+/**
+ * Submits the given group form's request in Javascript, allowing for in-place
+ * updating of the page.
+ * @param {HTMLFormElement} elem
+ * @param type the type of the schedulable, e.g. 'deadline', 'milestone', or 'event'
+ */
+function sendGroupFormViaAjax(elem) {
+    // Delete any pre-existing errors on the form
+    hideErrorBoxes(elem);
+
+    const formData = new FormData(elem);
+    const formRequest = new XMLHttpRequest();
+    let url = elem.getAttribute('data-url');
+    formRequest.open("POST", url);
+
+    formRequest.onload = () => {
+        if (formRequest.status === 200) {
+            window.location.reload();
+        } else {
+            const errors = formRequest.responseText.split('\n');
+            for (let errorMsg of errors) {
+                // Determine correct error field. Defaults to NameFeedback
+                let field = "shortName";
+                // Need to write error test for long name
+
+                field += 'Feedback';
+                const errorBox = elem.querySelector(`[id*="` + field + `"]`);
+                errorBox.textContent = errorMsg;
+                errorBox.style.display = 'block';
+            }
+        }
+    }
+    formRequest.send(formData);
+}
