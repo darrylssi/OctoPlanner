ba9be6d552006fb33ce01f102a808cc2953a0f47
Jonathan Tomlinson <jdt59@uclive.ac.nz>
2022-09-28 15:31:33 +1300

    IntelliJ suggestions for tidying code

--- portfolio/src/main/resources/static/scripts/groups.js
+++ portfolio/src/main/resources/static/scripts/groups.js
@@ -2,9 +2,8 @@
  * Swaps the visible and hidden text attributes for the specified group's show/hide users button.
  * @param button the button element to swap text for
  */
-function toggleUsersButton(button) {
-    let currentText = button.innerHTML;
-    let hiddenText = button.getAttribute("showText")
-    button.innerHTML=hiddenText;
+function toggleUsersButtonBottom(button) {
+    const currentText = button.innerHTML;
+    button.innerHTML=button.getAttribute("showText");
     button.setAttribute("showText", currentText);
 }
